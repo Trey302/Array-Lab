9de37bfd05b167cc005714fc8cac6d93d912a5db
Route9Library <you@example.com>
2020-11-24 18:02:00 -0500

    excercise 2

---
+++ numbers.js
@@ -0,0 +1,6 @@
+let points = [10254, 11203, 9546, 15999, 12253, 10256];
+// ascending order.
+points.sort(function(a, b){return a-b});
+// descending order.
+points.sort(function(a, b){return b-a});
+
