58db603e2235c032d954fcd5e0453bbbd298d480
Route9Library <you@example.com>
2020-11-24 17:41:27 -0500

    excercise 1

--- prompt.js
+++ prompt.js
@@ -7,3 +7,13 @@ console.log(songs);
 
 let jamFour = songs[3];
 songs.push("3 Headed Goat");
+songs.pop();
+
+console.log(songs.length);
+
+for (let i = 0; i < songs.length; i++) {
+    console.log(`${i}, ${songs[i]}`)
+};
+
+let lastSong = songs[songs.length - 1];
+console.log(lastSong);
