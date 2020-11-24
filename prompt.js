28d4acb3b85f12c1d55b503ffe0858b221e05b97
Route9Library <you@example.com>
2020-11-24 17:32:12 -0500

    start

---
+++ prompt.js
@@ -0,0 +1,9 @@
+let songs = ["Envy Me", "Emtionally Scarred", "24", "Effortless", "20/20"];
+console.log(songs[0])
+console.log(songs[2])
+
+songs.splice(4, 1, "Happy");
+console.log(songs);
+
+let jamFour = songs[3];
+songs.push("3 Headed Goat");
